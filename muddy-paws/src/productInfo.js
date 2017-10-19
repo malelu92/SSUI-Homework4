e895c4b8009af26de2bb04d7572dd8a24e3ecfd9
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2017-10-19 16:16:35 -0400

    organizing spec prods images

--- muddy-paws/src/productInfo.js
+++ muddy-paws/src/productInfo.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css'
 
+import dogHarnessOne from './images/dog_harness1.jpg';
+import dogHarnessOneTwo from './images/dog_harness1_2.jpg';
+import dogHarnessOneThree from './images/dog_harness1_3.jpg';
+
 class ProductInfo extends Component {
   constructor(props) {
 	super(props);
@@ -9,8 +13,15 @@ class ProductInfo extends Component {
   render() {
 	return (
 	  <div>
-		  <p>info</p>
-		</div>
+		<div className={"screen-spec-prod-l"}>
+    	  <img className={"prod-img-sel"} src={dogHarnessOne} alt="orange dog harness"></img>
+    	  <div className={"prod-group-cards"}>
+      	  	<img className={"prod-mini-cards"} src={dogHarnessOne} alt="orange dog harness"></img>
+      		<img className={"prod-mini-cards"} src={dogHarnessOneTwo} alt="orange dog harness"></img>
+      		<img className={"prod-mini-cards"} src={dogHarnessOneThree} alt="orange dog harness"></img>
+    	  </div>
+  		</div>
+	  </div>
 	);
   }
 }
