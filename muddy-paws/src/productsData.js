0b75b1903ca121fb74eb71adebd5dccd92523268
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2017-10-21 15:14:20 -0400

    created file with products data

---
+++ muddy-paws/src/productsData.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css'
+
+class ProductsData {
+  constructor(){
+    var data = require('./data.json');
+    this.images = data.images;
+    this.names = data.names;
+    this.descriptions = data.descriptions;
+    this.prices = this.prices;
+  }
+}
+
+export default ProductsData;
