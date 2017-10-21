c5154adb0233cab3acb473a9229ef1cd3bc00922
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2017-10-21 12:49:43 -0400

    adding image of selected items to cart window

--- muddy-paws/src/cartHover.js
+++ muddy-paws/src/cartHover.js
@@ -8,18 +8,25 @@ class CartHover extends Component {
 
   render() {
   	var totalPrice = 0;
+  	var items = [];
     if(this.props.cartProdQtys) {
       for (var i=0; i < this.props.cartProdQtys.length; i++) {
         totalPrice += this.props.cartProdQtys[i]*this.props.cartItems[i][3];
-      }
+
+      	items.push(    
+      	  <div className={"screen-align"}>
+            <img className="prod-img" src={this.props.cartItems[i][0]} />
+          </div>
+        );
+  	  }
     }
-	return (
-	  <div id="cart-window">
-	  	<p> Total price: {totalPrice}</p>
-	  <img className="prod-img" src={this.props.image} />
-	  </div>
-	);
-  }
+  return (
+	<div id="cart-window">
+	  <p> Total price: {totalPrice}</p>
+	  {items}
+	</div>
+  );
+}
 }
 
 
