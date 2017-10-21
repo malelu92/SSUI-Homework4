9d32464f4e0cc2b37ddf4f4c8971b2c50af9634d
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2017-10-21 13:07:49 -0400

    organized a bit of css

--- muddy-paws/src/cartHover.js
+++ muddy-paws/src/cartHover.js
@@ -7,15 +7,21 @@ class CartHover extends Component {
   }
 
   render() {
-  	var totalPrice = 0;
+  	var totalPrice = 0.0;
+  	var itemTotalPrice = 0;
   	var items = [];
     if(this.props.cartProdQtys) {
       for (var i=0; i < this.props.cartProdQtys.length; i++) {
-        totalPrice += this.props.cartProdQtys[i]*this.props.cartItems[i][3];
+      	itemTotalPrice = this.props.cartProdQtys[i]*this.props.cartItems[i][3];
+        totalPrice += itemTotalPrice;
 
       	items.push(    
       	  <div className={"screen-align"}>
             <img id="prod-img-cart" src={this.props.cartItems[i][0]} />
+            <div id="prod-info-cart">
+              <p>items: {this.props.cartProdQtys[i]}</p>
+              <p>price: {itemTotalPrice}</p>
+            </div>
           </div>
         );
   	  }
